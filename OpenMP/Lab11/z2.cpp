441847c49b8499d90a782f680eae83ba544f96bf
Piotr Janiszewski <idos2772@gmail.com>
2025-01-07 10:18:03 +0100

    Uzupelnienie metod

--- OpenMP/Lab11/z2.cpp
+++ OpenMP/Lab11/z2.cpp
@@ -14,6 +14,22 @@ using namespace std;
     wektor d = Macierz F * wektor c
     skalar B += wektor d * c
 */
+
+// Wielkosc
+const int N = 500;
+
+// Uzupelnienie macierzy, wektora i skalara
+void fillRandom(double** matrix, double* vector, int scalar, int size){
+    for(int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            matrix[i][j] = rand() % 10;
+        }
+        vector[i] = rand() % 10;
+    }
+    scalar = rand() % 10;
+}
+
+// Mnozenie macierzy przez skalar
 void matrixMullScalar(double mat[N][N], double scalar, double result[N][N]) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -22,12 +38,18 @@ void matrixMullScalar(double mat[N][N], double scalar, double result[N][N]) {
     }
 }
 
+// Mnozenie wektorow
+void vectorMullVector(double vec1[N], double vec2[N], double result) {
+    result = 0.0;
+    for (int i = 0; i < N; ++i) {
+        result += vec1[i] * vec2[i];
+    }
 }
-void vectorMullVector() {
-
-}
-void scalarMullVector() {
-
+// Mnozenie wektora przez skalar
+void scalarMullVector(double scalar, double vector[N], double result[N]) {
+    for (int i = 0; i < N; i++) {
+        result[i] = vector[i] * scalar;
+    }
 }
 // Mnożenie macierzy przez wektor
 void matrixMullVector(double mat[N][N], double vec[N], double result[N]) {
@@ -38,12 +60,15 @@ void matrixMullVector(double mat[N][N], double vec[N], double result[N]) {
         }
     }
 }
-void vectorMullVector() {
-
+// Odejmowanie macierzy
+void matrixOddMatrix(double mat1[N][N], double mat2[N][N], double result[N][N]) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            result[i][j] = mat1[i][j] - mat2[i][j];
+        }
+    }
 }
-void matrixOddMatrix() {
-
-}
+// Dodawanie skalara do wektora
 void scalarAddVector() {
 
 }
@@ -57,20 +82,25 @@ void vectorAddVector(double vec1[N], double vec2[N], double result[N]) {
 void dzialanieSekwencyjne() {
     for(int i = 0; i < 5; i++) {
         //mF = (sB * mF) - mA;
-        
+        matrixMullScalar(mF, sB, temp);
+        matrixOddMatrix(temp, mA, mF);
 
         //sA = wA * wD;
-        //sA = vectorMullVector(wA, wD);
+        vectorMullVector(wA, wD, sA);
         
         //wC = (sA * wB) + wA;
-        //wC = 
+        scalarMullVector(sA, wB, temp);
+        vectorAddVector(temp, wA, wC);
         
         // wD = mF * wC;
         matrixMullVector(mF, wC, wD);
         
         //sB += wD * wC;
-        //sB += vectorMullVector(wD, wC);
+        vectorMullVector(wD, wC, temp);
+        sB += temp;
     }
 }
 
-int main
+int main() {
+
+}
